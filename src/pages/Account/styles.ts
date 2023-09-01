1038ddca15ef2c3a52cf4a669e9acdbe86d12877
Stephanie Nascimento <ste_phaniie@hotmail.com>
2023-09-01 17:13:44 -0300

    criado tela de Account

---
+++ src/pages/Account/styles.ts
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import styled from 'styled-components/native';
+import { colors } from '../../theme/colors';
+
+export const Container = styled(View)`
+    display: flex;
+    flex: 1;
+    background-color: ${colors.BACKGROUND};
+    
+`;
---
+++ src/pages/Account/types.ts
@@ -0,0 +1,3 @@
+export interface AccountProps {
+    title?: string;
+}
