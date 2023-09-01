e88c9387429f6ee5733b35361eab4a7dcdf63372
Stephanie Nascimento <ste_phaniie@hotmail.com>
2023-09-01 17:11:26 -0300

    adicionado componente de header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { HeaderProps } from './types';
+
+export function Header({
+    title,
+    icon,
+    onPress,
+}: HeaderProps) {
+
+    return (
+        <View style={{ alignItems: 'flex-start', marginLeft: 10 }}>
+            {icon && (
+                <TouchableOpacity onPress={onPress} style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <Ionicons name="arrow-back" size={20} color="black" />
+                    <Text style={{ fontSize: 18, fontWeight: 'bold', paddingTop: 10, paddingBottom: 10, left: 10 }}>{title}</Text>
+                </TouchableOpacity>
+            )}
+        </View>
+    );
+};
